73f6a0a854883ad5f28ddbb1fd8b4dd0b8b3f3b7
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-02 20:37:35 -0300

    chore: install react-native-config and react-navigation

--- App.tsx
+++ App.tsx
@@ -1,2 +1,16 @@
-import StorybookUIRoot from "./.ondevice/Storybook";
-export { StorybookUIRoot as default };
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import StorybookUIRoot from './.ondevice/Storybook';
+import Config from 'react-native-config';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>oi</Text>
+    </NavigationContainer>
+  );
+};
+
+export default Config.LOAD_STORYBOOK === 'true' ? StorybookUIRoot : App;
