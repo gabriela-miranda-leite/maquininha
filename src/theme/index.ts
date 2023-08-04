4bc83303c21a3a623fd5ba69c56465a314b525eb
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-03 21:39:55 -0300

    style: create initial theme

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  colors: {
+    background: '#',
+    primary: '#E7B812',
+    primaryDark: '#C47F17',
+  },
+  fonts: {
+    primary: {
+      bold: 'Inter-Bold',
+      extraLight: 'Inter-ExtraLight',
+      light: 'Inter-Light',
+      medium: 'Inter-Medium',
+      regular: 'Inter-Regular',
+      semiBold: 'Inter-SemiBold',
+    },
+    sizes: {
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+  },
+};
