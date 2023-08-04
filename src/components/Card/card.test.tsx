7b10b075931bf308c6d7558920ab3cfb2114a685
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-04 18:22:21 -0300

    test: create initial card tests

---
+++ src/components/Card/card.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {screen, render} from '@testing-library/react-native';
+
+import {Card, CardProps} from '.';
+
+const defaultProps: CardProps = {
+  title: 'Maquininha T1',
+  description: 'À partir de 12x de R$ 1,90',
+};
+
+const create = (props = defaultProps) => render(<Card {...props} />);
+
+describe('Card', () => {
+  it('render correctly Card component', () => {
+    create();
+    const CardComponent = screen.queryByTestId('card');
+
+    expect(CardComponent).toBeTruthy();
+  });
+});
