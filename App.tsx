b6d5253437e4c01fabbe9ee1d818dac7a85fd992
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-05 14:30:27 -0300

    refactor: adjust config firebase

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import * as React from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import StorybookUIRoot from './.ondevice/Storybook';
 import Config from 'react-native-config';
 
 import {Card, Info} from './src/components';
+import database from '@react-native-firebase/database';
 
 const App = () => {
+  database()
+    .ref('/machines')
+    .on('value', snapshot => {
+      console.log('User data: ', snapshot.val());
+    });
+
   return (
     <NavigationContainer>
       <Card title="Maquininha T1" description="À partir de 12x de R$ 1,90" />
