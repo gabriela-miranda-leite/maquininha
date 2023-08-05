22ab3990ee419434d42515e2fc4f442432c64fbf
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-05 15:31:56 -0300

    refactor: removed services

--- App.tsx
+++ App.tsx
@@ -5,15 +5,8 @@ import StorybookUIRoot from './.ondevice/Storybook';
 import Config from 'react-native-config';
 
 import {Card, Info} from './src/components';
-import database from '@react-native-firebase/database';
 
 const App = () => {
-  database()
-    .ref('/machines')
-    .on('value', snapshot => {
-      console.log('User data: ', snapshot.val());
-    });
-
   return (
     <NavigationContainer>
       <Card title="Maquininha T1" description="À partir de 12x de R$ 1,90" />
