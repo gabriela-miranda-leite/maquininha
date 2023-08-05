2879c75af10f0d653476d27ed5fa1edf611b0ea4
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-05 19:14:47 -0300

    refactor: adjust return in getAllByMachines

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,19 @@
 import database from '@react-native-firebase/database';
 
-export const getAllByMachines = () =>
-  database()
-    .ref('/machines')
-    .on('value', snapshot => {
-      return snapshot.val();
-    });
+export const getAllByMachines = () => {
+  return new Promise((resolve, reject) => {
+    database()
+      .ref('/machines')
+      .once('value')
+      .then(snapshot => {
+        resolve(snapshot.val());
+      })
+      .catch(error => {
+        console.error('Erro ao buscar dados: ', error);
+        reject(error);
+      });
+  });
+};
 
 export const getByMachine = (id: string) =>
   database()
