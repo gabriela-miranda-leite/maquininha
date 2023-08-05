cf157add677cbc0b5aa20fc8ba3acbdc809fbff2
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-04 21:58:44 -0300

    feat: add info

--- App.tsx
+++ App.tsx
@@ -4,12 +4,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import StorybookUIRoot from './.ondevice/Storybook';
 import Config from 'react-native-config';
 
-import {Card} from './src/components';
+import {Card, Info} from './src/components';
 
 const App = () => {
   return (
     <NavigationContainer>
       <Card title="Maquininha T1" description="À partir de 12x de R$ 1,90" />
+      <Info description="testando hehe" icon="wifi" />
     </NavigationContainer>
   );
 };
