a1ae5d620fa35fbdb979378531a6d602f3a224ee
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-05 19:14:07 -0300

    feat: create machine page

--- src/screens/Machines/index.tsx
+++ src/screens/Machines/index.tsx
@@ -1,4 +1,44 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ListRenderItem} from 'react-native';
 
-export const Machines = () => <Text>Machines</Text>;
+import {Card} from '../../components';
+import {getAllByMachines} from '../../services';
+
+import {DataProps} from './machines.types';
+import * as S from './styles';
+
+export const Machines = () => {
+  const [data, setData] = useState<DataProps[]>([]);
+
+  useEffect(() => {
+    getAllByMachines()
+      .then(response => {
+        if (Array.isArray(response)) {
+          setData(response);
+        }
+      })
+      .catch(error => console.error('Erro ao obter dados: ', error));
+  }, []);
+
+  const renderHeader = () => (
+    <S.Header>
+      <S.Title>Escolha uma opção</S.Title>
+      <S.Description>Descubra a melhor opção para o seu negócio.</S.Description>
+    </S.Header>
+  );
+
+  const renderItem: ListRenderItem<DataProps> = ({item}) => (
+    <Card title={item.name} description={item.description} />
+  );
+
+  return (
+    <S.MachinesWrapper>
+      <S.Machines
+        data={data}
+        renderItem={renderItem}
+        ListHeaderComponent={renderHeader}
+        showsVerticalScrollIndicator={false}
+      />
+    </S.MachinesWrapper>
+  );
+};
