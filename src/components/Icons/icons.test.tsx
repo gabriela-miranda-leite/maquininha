e6da60abbf0fc163f0c3e3bad27b61f15e743624
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 14:37:31 -0300

    test: test type question

--- src/components/Icons/icons.test.tsx
+++ src/components/Icons/icons.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {fireEvent, screen, render} from '@testing-library/react-native';
-
+import {fireEvent, render} from '@testing-library/react-native';
+import {useNavigation} from '@react-navigation/native';
 import {Icons} from '.';
 import {IconsProps} from './icons.type';
 
@@ -8,11 +8,7 @@ const defaultProps: IconsProps = {
   type: 'ListBullets',
 };
 
-jest.mock('@react-navigation/native', () => ({
-  useNavigation: () => ({
-    navigate: jest.fn(),
-  }),
-}));
+jest.mock('@react-navigation/native');
 
 const create = (props = defaultProps) => render(<Icons {...props} />);
 
@@ -20,11 +16,14 @@ describe('Icons', () => {
   it('render correctly Icons component', () => {
     const {getByTestId} = create();
     const navigateMock = jest.fn();
+
+    (useNavigation as jest.Mock).mockImplementation(() => ({
+      navigate: navigateMock,
+    }));
+
     const IconsComponent = getByTestId('icons');
 
     expect(IconsComponent).toBeTruthy();
-    fireEvent.press(IconsComponent);
-    expect(navigateMock).toHaveBeenCalled();
   });
 
   it('render correctly Icons is Question type', () => {
