80d459d333fd0798143399601feaf5c2043db968
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 07:33:39 -0300

    feat: create machineInfo

--- src/screens/MachineInfo/index.tsx
+++ src/screens/MachineInfo/index.tsx
@@ -1,4 +1,33 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
 
-export const MachineInfo = () => <Text>MachineInfo</Text>;
+import {IconDetail, SlideImage} from '../../components';
+import {getByMachine} from '../../services';
+
+import {DataProps, MachineInfoProps} from './machineInfo.types';
+import * as S from './styles';
+
+export const MachineInfo: React.FC<MachineInfoProps> = ({route}) => {
+  const [data, setData] = useState<DataProps>();
+  const {id} = route.params;
+
+  useEffect(() => {
+    getByMachine(id)
+      .then(response => {
+        setData(response as DataProps);
+      })
+      .catch(error => console.error('Erro ao obter dados: ', error));
+  }, [id]);
+
+  return (
+    <S.MachineInfo>
+      <S.Title>{data?.name}</S.Title>
+      {data && <SlideImage images={data.showImages} />}
+      <S.DetailsWrapper>
+        {data &&
+          data.benefits.map(({type, description}, index) => (
+            <IconDetail type={type} description={description} key={index} />
+          ))}
+      </S.DetailsWrapper>
+    </S.MachineInfo>
+  );
+};
--- src/screens/MachineInfo/styles.ts
+++ src/screens/MachineInfo/styles.ts
@@ -1,4 +1,22 @@
 import styled from 'styled-components/native';
 import {defaultTheme} from '../../theme';
 
-export const MachineInfo = styled.View``;
+export const MachineInfo = styled.ScrollView`
+  flex: 1;
+  padding: ${defaultTheme.spacing.md};
+`;
+
+export const Title = styled.Text.attrs({numberOfLines: 1})`
+  color: ${defaultTheme.colors.text.dark};
+  font-family: ${defaultTheme.fonts.family.primary};
+  font-size: ${defaultTheme.fonts.sizes.xl};
+  font-weight: ${defaultTheme.fonts.weight.light};
+  margin-bottom: ${defaultTheme.spacing.xl};
+  text-align: center;
+`;
+
+export const DetailsWrapper = styled.View`
+  margin-bottom: ${defaultTheme.spacing.xl};
+  padding: ${defaultTheme.spacing.xl} 0;
+  row-gap: ${defaultTheme.spacing.md};
+`;
