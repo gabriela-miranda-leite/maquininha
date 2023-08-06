3a4360aa1245f9b529d9f74d1d4d64987925145c
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 09:55:45 -0300

    refactor: removed component not use

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 import {Card} from './Card';
 import {IconDetail} from './IconDetail';
 import {Icons} from './Icons';
-import {Info} from './Info';
 import {SlideImage} from './SlideImage';
 
-export {Card, IconDetail, Icons, Info, SlideImage};
+export {Card, IconDetail, Icons, SlideImage};
