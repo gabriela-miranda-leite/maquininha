6ed4c7551ac2378b1bd7ea703cf4daf40d902e35
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 05:56:23 -0300

    feat: add SlideImage

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import {Card} from './Card';
+import {IconDetail} from './IconDetail';
 import {Icons} from './Icons';
 import {Info} from './Info';
+import {SlideImage} from './SlideImage';
 
-export {Card, Icons, Info};
+export {Card, IconDetail, Icons, Info, SlideImage};
