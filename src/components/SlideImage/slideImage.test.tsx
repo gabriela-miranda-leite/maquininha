610f7262bab73aa794d6e878e44abd70ed267e7f
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 14:07:48 -0300

    test: adding testing click

--- src/components/SlideImage/slideImage.test.tsx
+++ src/components/SlideImage/slideImage.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {screen, render} from '@testing-library/react-native';
+import {fireEvent, screen, render} from '@testing-library/react-native';
 
 import {SlideImage} from '.';
 import {SlideImageProps} from './slideImage.type';
@@ -20,5 +20,17 @@ describe('SlideImage', () => {
     const SlideImageComponent = screen.queryByTestId('slideImage');
 
     expect(SlideImageComponent).toBeTruthy();
+  });
+
+  it('press image', () => {
+    const {getAllByTestId, getByTestId} = create();
+    const optionImage = getAllByTestId('optionImage');
+    fireEvent.press(optionImage[1]);
+
+    const mainImage = getByTestId('mainImage');
+
+    expect(mainImage.props.source).toEqual({
+      uri: 'https://res.cloudinary.com/dunz5zfpt/fl_progressive/f_auto,c_limit,w_384,q_auto/site-ton/maquininhas/machine-t2-2',
+    });
   });
 });
