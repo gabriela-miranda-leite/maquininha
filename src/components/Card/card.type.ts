8cdcc549c70e6c2e5912e6053441363e8c9bf9f4
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 07:31:39 -0300

    refactor: moved type react navigation

--- src/components/Card/card.type.ts
+++ src/components/Card/card.type.ts
@@ -1,3 +1,11 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../routes';
+
+export type ProfileScreenProps = NativeStackNavigationProp<
+  RootStackParamList,
+  'MachineInfo'
+>;
+
 export interface CardProps {
   title: string;
   id: string;
--- src/components/Icons/icons.type.ts
+++ src/components/Icons/icons.type.ts
@@ -1,3 +1,11 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../routes';
+
+export type ProfileScreenProps = NativeStackNavigationProp<
+  RootStackParamList,
+  'Machines'
+>;
+
 type IconsType = 'ListBullets' | 'Question';
 
 export interface IconsProps {
