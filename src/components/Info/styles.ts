475cc498256e95bcb1a23557e006aec36934f66c
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 09:55:11 -0300

    refactor: removed unnecessary

--- src/components/Info/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-import {defaultTheme} from '../../theme';
-
-export const Info = styled.View``;
-
-export const Description = styled.Text.attrs({numberOfLines: 1})`
-  font-family: ${defaultTheme.fonts.family.primary};
-  font-weight: ${defaultTheme.fonts.weight.medium};
-`;
