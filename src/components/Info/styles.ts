5e95374fae4b75c9c91e4f9a0f34fc3f49249ad4
Gabriela <gabrielamiranda11.10@gmail.com>
2023-08-06 07:32:35 -0300

    style: removed lib no use

--- src/components/Info/styles.ts
+++ src/components/Info/styles.ts
@@ -1,13 +1,9 @@
 import styled from 'styled-components/native';
 import {defaultTheme} from '../../theme';
 
-import Ionicons from 'react-native-vector-icons/Feather';
-
 export const Info = styled.View``;
 
-export const Description = styled.Text`
+export const Description = styled.Text.attrs({numberOfLines: 1})`
   font-family: ${defaultTheme.fonts.family.primary};
   font-weight: ${defaultTheme.fonts.weight.medium};
 `;
-
-export const Icon = styled(Ionicons)``;
